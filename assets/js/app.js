6baeef37ade9525bfab64053b1009968a9fd6e78
larry <ladams1776@yahoo.com>
2017-03-11 12:31:45 -0500

    spread operator allows us to push an array onto another array, but to insure that it is not a double array, but just a single one.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,20 @@
-var register = (email, name, lname) => {
-    console.log('Registered', email, name, lname)
-}
-
-var payload = ['alex@codeCourse', 'larry', 'adams'];
-
+var posts = [
+    {title: 'Post one', body: 'Lorem Ipsum'},
+    {title: 'Post two', body: 'Lorem Ipsum'}
+]
 
 
-register(...payload);
+
+posts.push({title: 'Post three', body: 'Lorem Ipsum'});
+
+
+var newPosts = [
+    {title: 'Post four', body: 'Lorem Ipsum'},
+    {title: 'Post five', body: 'Lorem Ipsum'}
+]
+
+
+posts.push(...newPosts);
+
+
+console.log(posts);
